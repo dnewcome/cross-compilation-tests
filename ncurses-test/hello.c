62f9a9b61435c7b5acfa5a72bee7a2e854e8057b
Dan Newcome <dan@dnuke.com>
2022-01-27 12:46:21 -0800

    update defines

--- ncurses-test/hello.c
+++ ncurses-test/hello.c
@@ -1,3 +1,7 @@
+#define PDC_WIDE
+#undef PDC_FORCE_UTF8
+#undef CHTYPE_32
+
 #include <ncurses.h>
 
 int main()
@@ -6,7 +10,7 @@ int main()
 	printw("Hello World !!!");	/* Print Hello World		  */
 	refresh();			/* Print it on to the real screen */
 	getch();			/* Wait for user input */
-	//endwin();
+	endwin();
 
 	return 0;
 }
